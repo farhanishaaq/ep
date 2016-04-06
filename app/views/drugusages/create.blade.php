44bc3075565da187e9b12934eda30a068fdb3c18
waqas <waqas.khalid32@gmail.com>
2016-04-06 13:49:53 +0500

    commit for dosage form

--- app/views/drugusages/create.blade.php
+++ app/views/drugusages/create.blade.php
@@ -1,67 +1,87 @@
-@extends('drugusages.layouts.master')
+@extends('layouts.master')
 <!--========================================================
                           TITLE
 =========================================================-->
 @section('title')
-Add New Drug
+    Add New Drug
 @stop
 
-
-<!--========================================================
+@section('redBar')
+    <div class="user_logo">
+        <div class="header_1 wrap_3 color_3 login-bar">Easy Physician
+            {{--<div class="col-md-12 mL25 taL">Easy Physician</div>--}}
+        </div>
+    </div>
+@stop
+@section('sliderContent')
+    @stop
+            <!--========================================================
                           CONTENT
 =========================================================-->
-@section('content1')
-    <section id="content">
-        
-		<div class = "user_logo">
-			<div class="header_1 wrap_3 color_3" style="color: #fff; padding-top: 20px">
-                        Add Drug
-            </div>
-		</div>
-		<br><br><br>
-@stop
 
 
-@section('content2')
+@section('content')
 
-        @foreach($errors->all("<p class='error'>:message</p>") as $message)
-	    {{ $message }}
-		@endforeach
+    @foreach($errors->all("<p class='error'>:message</p>") as $message)
+        {{ $message }}
+    @endforeach
 
-		<br/>
-	   <center>
-            <div style="border: 4px solid #129894; width: 800px; border-radius: 10px; background-color: #EBEBEB">
+    <br/>
+    <center>
+        <div class="container">
 
-            {{ Form::open(array('action' => 'DrugusagesController@store', 'style' => 'padding: 40px', 'id' => 'regForm')) }}
-                <table width="621" height="300" border="0">
-              <tr>
-                <td width="272" height="55"><label>Drug Name*</label> </td>
-                <td width="333">
-                    {{ Form::input('text', 'drug_name', null, array('required' => 'true')) }}
-                </td>
+            {{ Form::open(array('action' => 'DrugusagesController@store',  'class' =>"form-horizontal w100p ", 'id' => 'regForm','onsubmit' => 'checkForm()')) }}
+            <h3 class="mT10 mB0 c3 taL">Add Drug Usages</h3>
+            <hr class="w95p fL mT0"/>
+            <p class="col-xs-12 fL taR">Required Fields <kbd>*</kbd></p>
+            <section class="form-Section col-md-12 h695 fL">
+                <div class="container w100p">
+                    <h3 class="mT10 mB0 c3 taL">Add Drug Usages</h3>
+                    <hr class="w95p fL mT0"/>
+                    <div class="form-group">
+                        <label class="col-xs-4 control-label asterisk fL">Drug Name*</label>
+
+                        <div class="col-xs-7 fL">
+                            <input type="text" id="drug_name" name="drug_name" required="true"
+                                   value="{{{ Form::getValueAttribute('name', null) }}}" class="form-control"
+                                   placeholder="Drug Name e.g Panadol">
+                            <span id="errorName" class="field-validation-msg"></span>
+                            {{--{{ Form::input('text', 'drug_name', null, array('required' => 'true')) }}--}}
+                        </div>
 
 
-                <tr>
-                <td width="272"><label>Usage Note:</label></td>
-                <td width="333" height="350">{{ Form::textarea('usage_note', null, array('rows' => '7', 'cols' => '20', 'placeholder' => 'note', "style" => "font-size: 1.2em; margin-top: 2px; resize: none;") ) }}</td>
-                </tr>
-                <tr> 
-                <td colspan="2"> 
-                    <input name="patient_id" type="hidden" value="{{ $patient_id }}">
-                    <center>
-                    <div class="btn-wrap">
-                        <a class="btn_3" href="javascript:document.getElementById('regForm').reset();" data-type="reset">Reset</a>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
-                        <input type="submit" value="Save" class="submit" />
                     </div>
-                </center>
-                </td>
-                </tr>
-            </table>
-            {{ Form::close() }}
-            </div>
-        </center>
-		
-		<br><br>
+                    <div class="form-group">
+                        <label class="col-xs-4 control-label asterisk fL">Usage Note*</label>
 
-      
+                        <div class="col-xs-7 fL">
+                            <textarea id="usage_note" name="usage_note" required="true"
+                                      value="{{{ Form::getValueAttribute('name', null) }}}" class="form-control"
+                                      placeholder="Enter Usage Detail For Medicine Here"></textarea>
+                            <span id="errorName" class="field-validation-msg"></span>
+                            {{--{{ Form::input('text', 'drug_name', null, array('required' => 'true')) }}--}}
+                        </div>
+                    </div>
+
+
+                    {{--</div>--}}
+                    {{--<td width="272"><label>Usage Note:</label></td>--}}
+                    {{--<td width="333"--}}
+                    {{--height="350">{{ Form::textarea('usage_note', null, array('rows' => '7', 'cols' => '20', 'placeholder' => 'note', "style" => "font-size: 1.2em; margin-top: 2px; resize: none;") ) }}</td>--}}
+                    {{--</tr>--}}
+                    <div class="col-xs-12 taR pR0 mT20">
+                        <input type="reset" id="reset" value="Reset" class="submit"/>
+                        <input type="submit" id="create" value="Save && Close" class="submit"/>
+                        <input type="submit" id="create" value="Save && Continue" class="submit"/>
+                        <input type="submit" id="cancel" value="Cancel" class="submit"/>
+                    </div>
+                </div>
+            </section>
+            {{ Form::close() }}
+        </div>
+    </center>
+
+    <br><br>
+
+
 @stop
