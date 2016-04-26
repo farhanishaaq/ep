6e8f2f0d906a00c6a316e54c4435042181a1a9f5
Waqas Akram <waqas.doit@gmail.com>
2016-04-26 18:35:04 +0500

    print prescription

--- app/views/admin/admin_home.blade.php
+++ app/views/admin/admin_home.blade.php
@@ -52,7 +52,7 @@ class="current"
 					<a class="green" href="vitalSign">Add Vital Signs</a>
 					<a class="ferozi" href="app_prescription">Prepare Prescription</a>
 					{{--<a class="green" href="app_proc"> Diagnostic Procedures	</a>--}}
-					<a class="blue" href="app_pres_print" >Print Prescription </a>
+					<a class="blue" href="printPrescription" >Print Prescription </a>
 					<a class="purple" href="app_tests">Manage Test Reports</a>
 					<a class="orange" href="app_test_print">Print Test Report</a>
   					<a class="pink" href="app_check_fee">Add Checkup Fee</a>
