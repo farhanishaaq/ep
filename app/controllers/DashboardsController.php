45c1e4f13518367d07da33d82e901e86eaf8a311
waqas <waqas.khalid32@gmail.com>
2016-06-27 17:16:28 +0500

    ssss

--- app/controllers/DashboardsController.php
+++ app/controllers/DashboardsController.php
@@ -1,23 +1,29 @@
 <?php
 use App\Globals\GlobalsConst;
-class DashboardsController extends \BaseController {
+
+class DashboardsController extends \BaseController
+{
 
 
-	public function showDashboard(){
-		$roleId = 0;
-		$roles = Auth::user()->roles;
-		if($roles->count()){
-			foreach ($roles as $r){
-				$roleId = $r->id;
-			}
-		}
-		switch($roleId){
-			case GlobalsConst::SUPER_ADMIN_ID:
-				return View::make('dashboards.super_home');
-			case GlobalsConst::COMPANY_ADMIN_ID:
-				return View::make('dashboards.super_home');
-			default:
-				echo 'default';
-		}
-	}
+    public function showDashboard()
+    {
+        $roleId = 0;
+        $userType = Auth::user()->user_type;
+//		$roles = Auth::user()->roles;
+//		if($user_types->count()){
+//			foreach ($user_types as $t){
+//				$roleId = $r->id;
+//			}
+//		}
+        switch ($userType) {
+            case GlobalsConst::SUPER_ADMIN:
+                return View::make('dashboards.super_home');
+            case GlobalsConst::ADMIN:
+                return View::make('dashboards.admin');
+            case GlobalsConst::EMPLOYEE:
+                return View::make('dashboards.employee');
+            default:
+                echo 'default';
+        }
+    }
 }
