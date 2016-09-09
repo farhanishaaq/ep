6f269ad417eacc2a9dc0e1e9ae5c578ca7233c79
Zeeshan <zeeeshan.shafique@gmail.com>
2016-09-10 01:07:33 +0500

    saveCategory, saveLocation, saveManufacturer, savePurchase, savePurchaseDetail,saveStock functions added in their respective models

--- app/models/MedicineMenufacturer.php
+++ app/models/MedicineMenufacturer.php
@@ -26,4 +26,38 @@ class MedicineMenufacturer extends \Eloquent {
 	{
 		return $this->hasMany('MedicineCategory');
 	}
+
+	public static function saveMedicineMenufacturer($data,$dataProcessType=GlobalsConst::DATA_SAVE){
+
+		$vRules = MedicineMenufacturer::$rules;
+		$response = null;
+		if($dataProcessType==GlobalsConst::DATA_SAVE){
+			$medicine_menufacturer = new MedicineMenufacturer();
+		}else{
+			$id = isset($data['medicine_menufacturerId']) ? $data['medicine_menufacturerId'] : '';
+			if($id != ""){
+				$medicine_menufacturer = MedicineMenufacturer::find($id);
+			}else{
+				return $response = ['success'=>false, 'error'=> true, 'message' => 'Medicine manufacturer record did not find for updation! '];
+			}
+		}
+
+		//*****Start Rules Validators
+		$validator = Validator::make($data, $vRules);
+		if ($validator->fails())
+		{
+			return ['success'=>false, 'error'=> true, 'validatorErrors'=>$validator->errors()];
+		}
+		//*****End Rules Validators
+
+		$medicine_menufacturer->name = $data['name'];
+		$medicine_menufacturer->email = $data['email'];
+		$medicine_menufacturer->cell = $data['cell'];
+		$medicine_menufacturer->phone  = $data['phone'];
+		$medicine_menufacturer->address  = $data['address'];
+		$medicine_menufacturer->description  = $data['description'];
+		$medicine_menufacturer->save();
+		$response = ['success'=>'true','error'=>'false','message'=>'Medicine manufacturer has been saved successfully!'];
+		return $response;
+	}
 }
--- app/models/MedicinePurchase.php
+++ app/models/MedicinePurchase.php
@@ -1,14 +1,14 @@
 <?php
 
 
+use App\Globals\GlobalsConst;
 
 class MedicinePurchase extends \Eloquent {
 	
-	protected $fillable = ['business_unit_id','manufacturer_id','unit_price','date'];
-
+	protected $fillable = ['business_unit_id','manufacturer_id','date'];
 	public static $rules = [
 		'business_unit_id' => 'required',
-		'unit_price' => 'required',
+		'menufacturer_id' => 'required',
 	];
 
 	public function purchaseDetails()
@@ -23,6 +23,53 @@ class MedicinePurchase extends \Eloquent {
 
 	public function manufacturer(){
 		return $this->belongsTo('MedicineManufacturer');
+	}
+
+	public static function saveMedicinePurchase($data,$dataProcessType=GlobalsConst::DATA_SAVE){
+
+		$vRules = MedicinePurchase::$rules;
+		$response = null;
+		if($dataProcessType==GlobalsConst::DATA_SAVE){
+			$medicine_purchase = new MedicinePurchase();
+		}else{
+			$id = isset($data['medicine_purchaseId']) ? $data['medicine_purchaseId'] : '';
+			if($id != ""){
+				$medicine_purchase = MedicinePurchase::find($id);
+				//Delete purchase Detail on purchase Edit
+				MedicinePurchaseDetail::where('purchase_id','=',$id)->delete();
+			}else{
+				return $response = ['success'=>false, 'error'=> true, 'message' => 'Medicine Purchase record did not find for updation! '];
+			}
+		}
+
+		//*****Start Rules Validators
+		$validator = Validator::make($data, $vRules);
+		if ($validator->fails())
+		{
+			return ['success'=>false, 'error'=> true, 'validatorErrors'=>$validator->errors()];
+		}
+		//*****End Rules Validators
+
+		$medicine_menufacturerId = isset($data['menufacturer_id']) ? $data['menufacturer_id'] : null;
+		$business_unitId= isset($data['business_unit_id']) ? $data['business_unit_id'] : null;
+
+		$medicine_purchase->menufacturer_id = $medicine_menufacturerId;
+		$medicine_purchase->business_unit_id = $business_unitId;
+
+		$medicine_purchase->date = $data['date'];
+		$medicine_purchase->save();
+
+		//passing dummy data to purchaseDetail for testing
+		//$data = ['MedicinePurchaseID'=>$medicine_purchase->id,'medicine_id'=>['0'=>1,'1'=>2],'unit_price'=>['0'=>10,'1'=>20],'quantity'=>['0'=>100,'1'=>200]];
+		$data['MedicinePurchaseID'] = $medicine_purchase->id;
+		$mpdResult = MedicinePurchaseDetail::saveMedicinePurchaseDetail($data,GlobalsConst::DATA_SAVE);
+
+		//passing dummy data to purchaseDetail for testing
+		$data 	   = ['medicine_id'=>1,'business_unit_id'=>1,'location_id'=>1,'minimum_quantity'=>5,'quantity'=>7];
+
+		$msResult  = MedicineStock::saveMedicineStock($data,GlobalsConst::DATA_SAVE);
+		$response  = ['success'=>'true','error'=>'false','message'=>'Medicine purchase has been saved successfully!'];
+		return $response;
 	}
 
 }
