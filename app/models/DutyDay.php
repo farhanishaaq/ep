5464247b9da3282c6c6b23deed43fe2027463049
Muhammad Rashid <m.rashid.se@gmail.com>
2016-10-29 19:22:12 +0500

    get change from in timeslot and dutydays from prescription branch

--- app/models/DutyDay.php
+++ app/models/DutyDay.php
@@ -5,14 +5,14 @@ class DutyDay extends \Eloquent {
 	// Add your validation rules here
 
 	public static $rules = [
-        'employee_id' => 'required',
-        'day' => 'required|unique:dutydays,day,NULL,id,employee_id,3',
+        'doctor_id' => 'required',
+        'day' => 'required|unique:duty_days,day,NULL,id,doctor_id,3',
         'start' => 'required',
         'end' => 'required',
 	];
 
 	// Don't forget to fill this array
-	protected $fillable = ['day', 'start', 'end', 'employee_id', 'clinic_id'];
+	protected $fillable = ['day', 'start', 'end', 'doctor_id'];
 
     public static function makeSlots($start_time, $end_time, $day_id, $emp_id){
         $timeSlot  = GlobalsConst::TIME_SLOT_INTERVAL * 60;
@@ -20,13 +20,14 @@ class DutyDay extends \Eloquent {
         $start = strtotime($start_time);
         $end = strtotime($end_time);
 
+
         while($start <= $end){
-            $timeslot = new Timeslot();
+            $timeslot = new TimeSlot();
             $timeslot->slot = date("H:i:s", $start);
             $timeslot->save();
-            $timeslot->dutyday_id = $day_id;
+            $timeslot->duty_day_id = $day_id;
             $timeslot->save();
-            $timeslot->employee_id = $emp_id;
+            $timeslot->doctor_id = $emp_id;
             $timeslot->save();
             $start += $timeSlot;
         }
@@ -37,26 +38,65 @@ class DutyDay extends \Eloquent {
         $start = strtotime($start_time);
         $end = strtotime($end_time);
 
-        TimeSlot::where('dutyday_id', '=', $day_id)->delete();
+        TimeSlot::where('duty_day_id', '=', $day_id)->delete();
 
         while($start <= $end){
             $timeslot = new TimeSlot();
             $timeslot->slot = date("H:i:s", $start);
             $timeslot->save();
-            $timeslot->dutyday_id = $day_id;
+            $timeslot->duty_day_id = $day_id;
             $timeslot->save();
             $start += $fifteen_mins;
         }
     }
 
-	// Relationships
-	public function employee()
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function doctor()
     {
-        return $this->belongsTo('Employee');
+        return $this->belongsTo('Doctor');
     }
 
-    public function timeslots()
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function timeSlots()
     {
-        return $this->hasMany('Timeslot');
+        return $this->hasMany('TimeSlot');
+    }
+
+    /**
+     * @param array|null $columns
+     * @return mixed
+     */
+    public function user(array $columns = null)
+    {
+        return $this->employee()->first(["id", "user_id"])->user()->first($columns);
+    }
+
+    public static function saveDutyDays($data,$dataProcessType=GlobalsConst::DATA_SAVE){
+        $doctorId = $data['doctor_id'];
+        $response = ['success'=>true,'error'=>false,'message' => 'Duty Days has been saved successfully'];
+        foreach (GlobalsConst::$DAYS as $d){
+            if(isset($data[$d]) && $data[$d] != ''){
+//                dd($d);
+//                dd($dutyDayData['start']);
+                $dutyDayData = (array)json_decode($data[$d]);
+                $dutyDayData['doctor_id'] = $doctorId;
+                self::$rules['day'] = 'required|unique:duty_days,day,NULL,id,doctor_id,'.$data['doctor_id'];
+                $validator = Validator::make($dutyDayData, self::$rules);
+                if($validator->fails()) {
+                    $response = ['success'=>false,'error'=>true,'message' => $validator->errors()];
+                    break;
+                }
+                $dutyDayData['start'] = date("H:i", strtotime($dutyDayData['start']));
+                $dutyDayData['end'] = date("H:i", strtotime($dutyDayData['end']));
+//                dd($dutyDayData);
+                $dutyDay = DutyDay::create($dutyDayData);
+                DutyDay::makeSlots($dutyDayData['start'], $dutyDayData['end'], $dutyDay->id, $dutyDayData['doctor_id']);
+            }
+        }
+        return $response;
     }
 }
--- app/models/TimeSlot.php
+++ app/models/TimeSlot.php
@@ -8,31 +8,51 @@ class TimeSlot extends \Eloquent {
 	];
 
 	// Don't forget to fill this array
-	protected $fillable = ['slot', 'reserved', 'dutyday_id', 'employee_id', 'clinic_id'];
+	protected $fillable = ['doctor_id', 'duty_day_id', 'slot'];
 
     // Relationships
-    public function dutyday()
+    public function dutyDay()
     {
-        return $this->belongsTo('Dutyday');
+        return $this->belongsTo('DutyDay');
     }
 
     public function appointments()
     {
-        return $this->hasMany('Appointment', 'timeslot_id', 'id');
+        return $this->hasMany('Appointment', 'time_slot_id', 'id');
     }
 
+
+    public static function fetchTimeSlotsByAppointmentDate(Appointment $appointment, $isList=false){
+        $appointmentDay = date('l',strtotime($appointment->date));
+        $dutyDayId = $appointment->doctor->dutyDays()->where('day','=',$appointmentDay)->first()->id;
+        $timeSlot = self::where('doctor_id','=',$appointment->doctor_id)
+            ->where('duty_day_id','=', $dutyDayId)
+            ->get();
+        if ($isList){
+            return $timeSlot->lists('slot', 'id');
+        }
+        return $timeSlot;
+    }
+
+
+    /**
+     * @param $doctorId
+     * @param $day
+     * @param bool $isLists
+     * @return mixed
+     */
     public static function fetchAvailableTimeSlots($doctorId,$day,$isLists=false){
-        $qry = DB::table('timeslots')
-            ->select(['timeslots.id','timeslots.slot'])
-            ->join('dutydays', 'dutydays.id', '=', 'timeslots.dutyday_id','inner')
-            ->where('dutydays.employee_id', '=', $doctorId)
-            ->where('dutydays.day', '=', $day)
-            ->whereNotIn('timeslots.id', function($query)
+        $qry = DB::table('time_slots')
+            ->select(['time_slots.id','time_slots.slot'])
+            ->join('duty_days', 'duty_days.id', '=', 'time_slots.duty_day_id','inner')
+            ->where('duty_days.doctor_id', '=', $doctorId)
+            ->where('duty_days.day', '=', $day)
+            ->whereNotIn('time_slots.id', function($query)
             {
 //                $query->select(DB::raw(1))
-                $query->select('appointments.timeslot_id')
+                $query->select('appointments.time_slot_id')
                     ->from('appointments')
-                    ->join('timeslots AS ts2', 'ts2.id', '=', 'appointments.timeslot_id','inner');
+                    ->join('time_slots AS ts2', 'ts2.id', '=', 'appointments.time_slot_id','inner');
 //                    ->whereRaw('orders.user_id = users.id');
             });
 //        $qry->toSql();
