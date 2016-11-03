605e3a618d5cf1ff0ce20d1f69d87b97f5c58243
Muhammad Rashid <m.rashid.se@gmail.com>
2016-11-04 00:39:51 +0500

    db password change

--- app/config/database.php
+++ app/config/database.php
@@ -57,7 +57,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'ep',
 			'username'  => 'root',
-			'password'  => '',
+			'password'  => 'srws@!',
 			'port'		=>'3306',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
