f39d0f0eaad1745b020bd962ef4bdc80c385acb3
ahtisham <ahtisham300@gmail.com>
2017-11-25 14:37:40 +0500

    fixed comment auth

--- app/models/Comment.php
+++ app/models/Comment.php
@@ -12,10 +12,12 @@ class Comment extends \Eloquent
 
     public function saveComment ($data){
         $dr_id=$data['id'];
+        $user_id=$data['user_id'];
         $content = $data['comment'];
 
 
         $this->doctor_id =$dr_id;
+        $this->patient_id =$user_id;
         $this->comments = $content;
         $this->save();
         return 'sucess';
