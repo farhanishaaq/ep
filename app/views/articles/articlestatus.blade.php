d2a3fcece8fd8f46c92bdea7a33f30021bc3eba3
farhancopyhouse <frhancopyhouse@gmail.com>
2018-01-06 12:36:18 +0500

    article resoved

--- app/views/articles/articlestatus.blade.php
+++ app/views/articles/articlestatus.blade.php
@@ -11,7 +11,7 @@ Manage Articles
 
 @section('redBar')
 <div class = "user_logo">
-    <div class="header_1 wrap_3 color_3 login-bar">Easy Physician
+    <div class="header_1 wrap_3 color_3 login-bar">Article Status
         {{--<div class="col-md-12 mL25 taL">Easy Physician</div>--}}
     </div>
 </div>
