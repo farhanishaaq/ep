f578c5bc3905d718a2ef90a361ec4012c097e42a
ahtisham <ahtisham300@gmail.com>
2018-03-13 11:46:02 +0500

    userType Solve

---
+++ app/database/migrations/2018_03_10_155041_create_skills_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Globals\GlobalsConst;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateSkillsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('skills', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string("name");
+            $table->enum('skill_type',array_keys(GlobalsConst::SKILLS_TABLE_ENUM))->nullable();
+			$table->timestamps();
+
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('skills');
+	}
+
+}
